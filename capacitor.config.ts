d324a7a318d71c2cd7e254c3cbeba8d3593f3807
joaoromao17 <vitor.romao0442@gmail.com>
2025-07-07 14:37:03 -0300

    Voltando a configuração antiga

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,31 +1,28 @@
 
-import type { CapacitorConfig } from '@capacitor/core';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.3b331ef64013442e8d178bbe98cc4352',
-  appName: 'casa-da-ben',
+  appId: 'com.icb610.app',
+  appName: 'Igreja Casa da Benção 610',
   webDir: 'dist',
-  bundledWebRuntime: false,
   plugins: {
-    Keyboard: {
-      resize: 'body',
-      style: 'dark',
-      resizeOnFullScreen: true,
+    FirebaseMessaging: {
+      presentationOptions: ['badge', 'sound', 'alert'],
     },
-    StatusBar: {
-      style: 'dark',
-      backgroundColor: '#000000',
+    CapacitorHttp: {
+      enabled: true,
     },
   },
   android: {
     allowMixedContent: true,
     captureInput: true,
     webContentsDebuggingEnabled: true,
+    appendUserAgent: 'CapacitorWebView',
   },
   ios: {
     contentInset: 'automatic',
     scrollEnabled: true,
-  },
+  }
 };
 
 export default config;
